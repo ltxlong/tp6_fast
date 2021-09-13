b3a364249a086b0812f22a14f5de72ad47064e81
ltxlong <416803647@qq.com>
2021-09-13 10:09:57 +0800

    封装commonTrait

---
+++ CommonTrait.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace app\common\traits;
+
+use app\BaseLogic;
+use app\BaseModel;
+use app\common\lib\other\DataEdit;
+
+/**
+ * 公用的trait
+ * @author longtaixuan
+ * @email 416803647@qq.com
+ */
+trait CommonTrait
+{
+    /**
+     * 获取DataEdit实例
+     * @param array $options
+     * @return DataEdit
+     */
+    public function dataEdit(array $options = []): DataEdit
+    {
+        return DataEdit::instance($options);
+    }
+
+    /**
+     * 创建class实例
+     * @param string $Class
+     * @return mixed
+     */
+    public function class(string $Class)
+    {
+        return app($Class);
+    }
+
+    /**
+     * 创建model实例
+     * @param string $modelClass
+     * @return BaseModel
+     */
+    public function model(string $modelClass)
+    {
+        return app($modelClass);
+    }
+
+    /**
+     * 创建logic实例
+     * @param string $logicClass
+     * @return BaseLogic
+     */
+    public function logic(string $logicClass)
+    {
+        return app($logicClass);
+    }
+
+    /**
+     * 获取参数转换
+     * @param $arr
+     * @return array
+     */
+    public function buildParam($arr)
+    {
+        $param = [];
+        if (is_array($arr)) {
+            foreach ($arr as $k => $v) {
+                $param[$k] = $this->request->param($v);
+            }
+        }
+
+        return $param;
+    }
+
+    /**
+     * 获取列表（分页）
+     * @param string $modelClass -- 模型路径
+     * @param array $where
+     * @param string $field
+     * @param string $order
+     * @param int $perPage -- 每页数量
+     * @return array
+     */
+    public function getListPage(
+        string $modelClass,
+        array $where = [],
+        string $field = '*',
+        string $order = 'id desc',
+        int $perPage = 10
+    ) {
+        $model = app($modelClass);
+
+        $query = $model->where($where)->field($field);
+
+        $count = $query->count();
+
+        return $query->order($order)
+            ->paginate(
+                $perPage,
+                $count
+            )->toArray();
+    }
+
+    /**
+     * 获取列表（不分页）
+     * @param string $modelClass -- 模型路径
+     * @param array $where
+     * @param string $field
+     * @param string $order
+     * @return array
+     */
+    public function getListOnly(
+        string $modelClass,
+        array $where = [],
+        string $field = '*',
+        string $order = 'id desc'
+    ) {
+        $model = app($modelClass);
+
+        $res = $model->where($where)->field($field)->order($order)->select();
+
+        if ($res->isEmpty()) {
+            return [];
+        } else {
+            return $res->toArray();
+        }
+    }
+
+}
