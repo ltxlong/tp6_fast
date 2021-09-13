deeb84c984e48f0e3e832a802f63aac9d9b13b59
ltxlong <416803647@qq.com>
2021-09-13 10:35:57 +0800

    封装BaseLogic的__get()

--- BaseLogic.php
+++ BaseLogic.php
@@ -12,5 +12,54 @@ use app\common\traits\CommonTrait;
  */
 class BaseLogic
 {
+    /**
+     * @param $name
+     * @return BaseModel|\think\App
+     * @author longtaixuan
+     * @email 416803647@qq.com
+     */
+    public function __get($name)
+    {
+        // 判断类别Model、Service
+        // 先看common前缀，再看后缀
+        // 有common的就去common找，非common的就先找当前应用
+        // 非common的要首字母大写转换
+        // 默认：
+        // 存放的文件夹是model、service
+        // 除了model文件，service文件名添加后缀Service
+
+        $isCommon = false;
+        if (strpos($name, 'common') === 0) {
+            $isCommon = true;
+        }
+
+        switch ($name) {
+            case strstr($name, 'Model') === 'Model':
+                $suffix = 'Model';
+                break;
+            case strstr($name, 'Service') === 'Service':
+                $suffix = 'Service';
+                break;
+            default:
+                $suffix = 'Model';
+        }
+
+        if ($isCommon) {
+            $name = str_replace(['common', $suffix], '', $name);
+            $classPath = 'app\common\\' . lcfirst($suffix) . '\\' . $name;
+        } else {
+            $name = str_replace($suffix, '', $name);
+            $name = ucfirst($name);
+            $module = app('http')->getName();
+            $classPath = 'app\\' . $module . '\\' . lcfirst($suffix) . '\\' . $name;
+        }
+
+        if ($suffix !== 'Model') {
+            $classPath .= $suffix;
+        }
+
+        return app($classPath);
+    }
+    
     use CommonTrait;
 }
